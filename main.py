ec5843c5ce784a9cd93d196cb5075cf4d264be8b
pateld <pateld@educ.college-montreal>
2022-11-18 15:47:51 -0500

    commit jeu de monstres

--- main.py
+++ main.py
@@ -33,25 +33,20 @@ def fight():
     """
     fight() est une fonction qui exécute/définit le code pour l'option 1, c'est-à-dire si le joueur combat le monstre
     """
-    global niveau_vie, numero_combat, nombre_victoires, victoires_consecutives, nombre_defaites, numero_adversaire
-    score_de = randint(10, 50) + randint(1, 5)
+    global niveau_vie, nombre_victoires, victoires_consecutives, nombre_defaites
+    score_de = randint(1, 5) + randint(1, 5)
     print(f"Vous avez lancé un: {score_de}")
+
     if score_de <= force_adversaire:
         niveau_vie -= force_adversaire
         print(f"Vous avez perdu! Vous avez maintenant {niveau_vie} vies.")
         nombre_defaites += 1
         victoires_consecutives = 0
-        if niveau_vie <= 0:
-            print(f"GAME OVER! Vous avez vaicu {nombre_victoires} monstres.")
-            restart()
     elif score_de > force_adversaire:
         niveau_vie += force_adversaire
         print(f"Vous avez gagné! Vous avez maintenant {niveau_vie} vies.")
         nombre_victoires += 1
         victoires_consecutives += 1
-    print(f"Niveau de vie de l’usager : {niveau_vie}")
-    print(f"Combat {numero_combat}  : {nombre_victoires} victoires vs {nombre_defaites} défaites")
-    print(f"victoires consécutives  :  {victoires_consecutives}")
 
 
 jeu_commence = True
@@ -66,7 +61,8 @@ while jeu_commence:
     print("2- Contourner cet adversaire et aller ouvrir une autre porte")
     print("3- Afficher les règles du jeu")
     print("4- Quitter la partie")
-    question_1 = int(input("Que voulez_vous faire:"))
+    choix_joueur = int(input("Que voulez_vous faire:"))
+
     if victoires_consecutives == 3:
         numero_combat += 1
         numero_adversaire += 1
@@ -76,20 +72,21 @@ while jeu_commence:
         victoires_consecutives = 0
         print(f"Vous tombez face à face avec un boss de difficulté {force_boss}")
         fight()
-    elif question_1 == 1:
+
+    if choix_joueur == 1:
         numero_combat += 1
         numero_adversaire += 1
         print(f"Adversaire : {numero_adversaire}")
         print(f"Force de l’adversaire : {force_adversaire}")
+        print(f"Niveau de vie de l’usager : {niveau_vie}")
+        print(f"Combat {numero_combat}  : {nombre_victoires} victoires vs {nombre_defaites} défaites")
+        print(f"victoires consécutives  :  {victoires_consecutives}")
         fight()
-    elif question_1 == 2:
+    elif choix_joueur == 2:
         niveau_vie -= 1
         print("Vous avez choisi d'éviter le combat, donc vous perdez une vie.")
         print(f"Vies:{niveau_vie}")
-        if niveau_vie <= 0:
-            print(f"GAME OVER! Vous avez vaicu {nombre_victoires} monstres.")
-            restart()
-    elif question_1 == 3:
+    elif choix_joueur == 3:
         print("Pour réussir un combat, il faut que la valeur du dé soit supérieure à la force de l’adversaire.")
         print("Dans ce cas, le niveau de vie de l’usager est augmenté de la force de l’adversaire.")
         print("Une défaite a lieu lorsque la valeur du dé est inférieure ou égale à la force de l’adversaire.")
@@ -98,6 +95,16 @@ while jeu_commence:
         print("L’usager peut combattre ou éviter chaque adversaire")
         print("Cependant, il y a une pénalité de 1 point de vie si vous évitez le combat.")
         nouveau_monstre = False
-    else:
+    elif choix_joueur == 4:
         print("Au revoir!")
         jeu_commence = False
+    else:
+        print("Cela n'est pas un choix")
+        nouveau_monstre = False
+
+    if niveau_vie <= 0:
+        """
+        Vérifier si joueur mort.
+        """
+        print(f"GAME OVER! Vous avez vaicu {nombre_victoires} monstres.")
+        restart()
